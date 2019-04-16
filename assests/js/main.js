c7f8c5f5b93b01f062a906356986c5d736e0c524
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-04-16 22:19:09 +0100

    Created html form for book inputs

--- assests/js/main.js
+++ assests/js/main.js
@@ -3,35 +3,27 @@ function Book(bookAuthor, bookTitle, bookPages, bookRead) {
   let title = bookTitle;
   let page = bookPages;
   let read = bookRead;
-
   this.getAuthor = function() {
     return author;
   };
-
   this.setAuthor = function(bookAuthor) {
     author = bookAuthor;
   };
-
   this.getTitle = function() {
     return title;
   };
-
   this.setTitle = function(bookTitle) {
     title = bookTitle;
   };
-
   this.getPage = function() {
     return page;
   };
-
   this.setPage = function(bookPages) {
     page = bookPages;
   };
-
   this.getRead = function() {
     return read;
   };
-
   this.setRead = function(bookRead) {
     read = bookRead;
   };
@@ -40,10 +32,8 @@ function Book(bookAuthor, bookTitle, bookPages, bookRead) {
 var util = {
   store: function(namespace, data) {
     if (arguments.length > 1) {
-      console.log("hello good people");
       return localStorage.setItem(namespace, JSON.stringify(data));
     } else {
-      console.log("hello all");
       var store = localStorage.getItem(namespace);
       return (store && JSON.parse(store)) || [];
     }
