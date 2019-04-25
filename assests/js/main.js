026a24a5ebdc417794863cd2ae209a0bc20e9b22
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-04-25 02:30:42 +0100

    Included a unique id generator

--- assests/js/main.js
+++ assests/js/main.js
@@ -30,6 +30,18 @@ function Book(bookAuthor, bookTitle, bookPages) {
 }
 
 var util = {
+  uuid: function () {
+    /* https://www.reddit.com/r/learnprogramming/
+    comments/7ovgad/using_bitwise_to_generate_a_uuid/ */
+    let i, random;
+		let uuid = '';
+		for (i = 0; i < 32; i++) {
+      random = Math.random() * 16 | 0;
+      if (i === 8 || i === 12 || i === 16 || i === 20) uuid += '-';
+			uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
+    }
+    return uuid;
+  },
   store: function(namespace, data) {
     if (arguments.length > 1) {
       return localStorage.setItem(namespace, JSON.stringify(data));
